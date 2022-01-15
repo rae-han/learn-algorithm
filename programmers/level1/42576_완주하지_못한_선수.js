6d92575c7a646520d357e3118272da4feb6998ea
raehan <raehan900809@gmail.com>
2022-01-15 20:48:26 +0900

    review: other code

--- programmers/level1/42576_완주하지_못한_선수.js
+++ programmers/level1/42576_완주하지_못한_선수.js
@@ -6,16 +6,24 @@ function solution(participant, completion) {
 
   // return participant.sort().find((value, index) => value !== completion[index], completion.sort());
   // return participant.find(name=>!completion[name]--,completion.map(name=>completion[name]=(completion[name]|0)+1));
-  return participant.find(name => { 
+  return participant.find(name => {
+    // console.log('completion', completion);
+    // console.log('completion[name]', completion[name]);
     return !completion[name]-- 
-  },completion.map(name=>completion[name]=(completion[name]|0)+1));
+  },completion.map(name=>{
+    console.log('completion', completion);
+    console.log('name', name)
+    console.log('completion[name]', completion[name]);
+    return completion[name]=(completion[name]|0)+1
+  }));
 
 }
 
 const testCase = [
-  {	participant: ["leo", "kiki", "eden"], completion: ["eden", "kiki"], result: "leo" },
-  {	participant: ["marina", "josipa", "nikola", "vinko", "filipa"], completion: ["josipa", "filipa", "marina", "nikola"], result: "vinko" },
-  {	participant: ["mislav", "stanko", "mislav", "ana"], completion: ["stanko", "ana", "mislav"], result: "mislav" },
+  // {	participant: ["leo", "kiki", "eden"], completion: ["eden", "kiki"], result: "leo" },
+  // {	participant: ["marina", "josipa", "nikola", "vinko", "filipa"], completion: ["josipa", "filipa", "marina", "nikola"], result: "vinko" },
+  // {	participant: ["mislav", "stanko", "mislav", "ana"], completion: ["stanko", "ana", "mislav"], result: "mislav" },
+  {	participant: ["a", "b", "c"], completion: ["a", "b", "a", "c"], result: "?" },
 ];
 
 testCase.forEach(({participant, completion, result}) => {
@@ -23,3 +31,9 @@ testCase.forEach(({participant, completion, result}) => {
   let res = solution(participant, completion);
   console.log(res);
 });
+
+// console.log('bit operation');
+// console.log(0|0);
+// console.log(0|1);
+// console.log(1|0);
+// console.log(1|1);
