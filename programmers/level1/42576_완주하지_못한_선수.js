27a0ec5dde1b9b196a8a1adb7ea60567b384c34a
raehan <raehan900809@gmail.com>
2022-01-15 20:56:35 +0900

    modify: algorithm

--- programmers/level1/42576_완주하지_못한_선수.js
+++ programmers/level1/42576_완주하지_못한_선수.js
@@ -11,19 +11,19 @@ function solution(participant, completion) {
     // console.log('completion[name]', completion[name]);
     return !completion[name]-- 
   },completion.map(name=>{
-    console.log('completion', completion);
-    console.log('name', name)
-    console.log('completion[name]', completion[name]);
-    return completion[name]=(completion[name]|0)+1
+    // console.log('completion', completion);
+    // console.log('name', name)
+    // console.log('completion[name]', completion[name]);
+    return completion[name]=(completion[name]??0)+1
   }));
 
 }
 
 const testCase = [
-  // {	participant: ["leo", "kiki", "eden"], completion: ["eden", "kiki"], result: "leo" },
-  // {	participant: ["marina", "josipa", "nikola", "vinko", "filipa"], completion: ["josipa", "filipa", "marina", "nikola"], result: "vinko" },
-  // {	participant: ["mislav", "stanko", "mislav", "ana"], completion: ["stanko", "ana", "mislav"], result: "mislav" },
-  {	participant: ["a", "b", "c"], completion: ["a", "b", "a", "c"], result: "?" },
+  {	participant: ["leo", "kiki", "eden"], completion: ["eden", "kiki"], result: "leo" },
+  {	participant: ["marina", "josipa", "nikola", "vinko", "filipa"], completion: ["josipa", "filipa", "marina", "nikola"], result: "vinko" },
+  {	participant: ["mislav", "stanko", "mislav", "ana"], completion: ["stanko", "ana", "mislav"], result: "mislav" },
+  {	participant: ["a", "b", "c", "d"], completion: ["a", "b", "a", "c"], result: "d" },
 ];
 
 testCase.forEach(({participant, completion, result}) => {
