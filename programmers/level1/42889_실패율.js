bdb058af5daf5b9c79ae630c7afd72f28f7db817
raehan <raehan900809@gmail.com>
2022-01-16 23:03:01 +0900

    add: failure ratio

--- programmers/level1/42889_실패율.js
+++ programmers/level1/42889_실패율.js
@@ -1,14 +1,23 @@
 function solution(N, stages) {
-  console.log(N, stages)
+  stages = stages.reduce((pre, cur) => {
+    console.log(pre, cur)
+    console.log(pre[cur])
+    pre[cur]=(pre[cur]|0)+1;
+    return pre;
+  }, {})
+  console.log(stages)
+  console.log('parmas', N, stages)
+  console.log(stages)
   return 1;
 }
 
 
 const testCases = [
-  { number: 5, stages: [2,1,2,6,2,4,3,3], result: [3,4,2,1,5]},
-  { number: 4, stages: [4,4,4,4,4], result: [4,1,2,3]},
+  { number: 5, stages: [2,1,2,6,2,4,3,3], result: [3,4,2,1,5] },
+  // { number: 4, stages: [4,4,4,4,4], result: [4,1,2,3] },
 ]
 
 testCases.forEach(({ number, stages, result }) => {
-  console.log(`result: ${result}, solution: ${solution(number, stages)}`)
+  const log = `result: ${result}, solution: ${solution(number, stages)}`;
+  // console.log(log)
 })
