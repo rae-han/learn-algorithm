5a2858a7da282af9614ebbef718ed44bbdb81fa6
raehan <raehan900809@gmail.com>
2022-01-16 14:28:30 +0900

    modify: testcase

--- programmers/level1/42889_실패율.js
+++ programmers/level1/42889_실패율.js
@@ -1,7 +1,14 @@
-
+function solution(N, stages) {
+  console.log(N, stages)
+  return 1;
+}
 
 
 const testCases = [
-  { number: 5, states: [2,1,2,6,2,4,3,3], result: [3,4,2,1,5]},
-  { number: 4, states: [4,4,4,4,4], result: [4,1,2,3]},
+  { number: 5, stages: [2,1,2,6,2,4,3,3], result: [3,4,2,1,5]},
+  { number: 4, stages: [4,4,4,4,4], result: [4,1,2,3]},
 ]
+
+testCases.forEach(({ number, stages, result }) => {
+  console.log(`result: ${result}, solution: ${solution(number, stages)}`)
+})
