84222884d5889f478ebc6709679330abcdd20f49
raehan <raehan900809@gmail.com>
2022-01-21 22:11:11 +0900

    add: map

--- programmers/level1/42889_실패율.js
+++ programmers/level1/42889_실패율.js
@@ -1,20 +1,33 @@
 function solution(N, stages) {
-  stages = stages.reduce((pre, cur) => {
-    console.log(pre, cur)
-    console.log(pre[cur])
-    pre[cur]=(pre[cur]|0)+1;
+  stages = stages.reduce((acc, cur) => {
+    console.log(acc, cur)
+    console.log(acc[cur])
+    acc[cur]=(acc[cur]|0)+1;
+    return acc;
+  }, [].length = 6);
+
+  console.log('######## reduce result ########')
+  console.log(stages)
+
+  console.log('######## reduceRight ########')
+  stages.reduceRight((pre, cur, idx) => {
+    console.log(`idx: ${idx}`)
+    if(idx!==N) {
+      console.log('not found complete')
+    }
+
     return pre;
-  }, {})
+  }, [])
   console.log(stages)
-  console.log('parmas', N, stages)
-  console.log(stages)
+  
   return 1;
 }
 
 
 const testCases = [
-  { number: 5, stages: [2,1,2,6,2,4,3,3], result: [3,4,2,1,5] },
-  // { number: 4, stages: [4,4,4,4,4], result: [4,1,2,3] },
+  // { number: 5, stages: [2,1,2,2,4,3,3], result: [3,4,2,1,5] },
+  // { number: 5, stages: [2,1,2,6,2,4,3,3], result: [3,4,2,1,5] },
+  { number: 4, stages: [4,4,4,4,4], result: [4,1,2,3] },
 ]
 
 testCases.forEach(({ number, stages, result }) => {
