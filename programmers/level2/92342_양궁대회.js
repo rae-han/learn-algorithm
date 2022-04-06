d801a3aef94f21eb14370d0130bbe3fddf1b7018
Raehan <raehan900809@gmail.com>
2022-04-06 21:27:50 +0900

    modify: complete exploreration

--- programmers/level2/92342_양궁대회.js
+++ programmers/level2/92342_양궁대회.js
@@ -1,55 +1,36 @@
 const testcases = [
-  // { n: 0, info: [0,0,0,0,0,0,0,0,0,0,0], result: [0,0,0,0,0,0,0,0,0,0,] },
-  { n: 10, info: [0,0,4,0,3,0,2,0,1,0,0], result: [1,1,0,1,0,1,3,1,2,0,] },
-  // { n: 5, info: [2,1,1,1,0,0,0,0,0,0,0], result: [0,2,2,0,1,0,0,0,0,0,0] },
+  // { n: 0, info: [0,0,0,0,0,0,0,0,0,0,0], result: [0,0,0,0,0,0,0,0,0,0,0] },
+  { n: 5, info: [2,1,1,1,0,0,0,0,0,0,0], result: [0,2,2,0,1,0,0,0,0,0,0] },
   // { n: 1, info: [1,0,0,0,0,0,0,0,0,0,0], result: [-1] },
   // { n: 9, info: [0,0,1,2,0,1,1,1,1,1,1], result: [1,1,2,0,1,2,2,0,0,0,0] },
   // { n: 10, info: [0,0,0,0,0,0,0,0,3,4,3], result: [1,1,1,1,1,1,1,1,0,0,2] },
-  // { n: 10, info: [0,0,0,0,0,0,0,0,3,4,3], result: [1,1,1,1,1,1,1,1,0,0,2] },
+  // { n: 8, info: [4,0,3,0,2,0,1,0,0,0,0], result: [0,1,0,1,3,1,2,0,0,0,0] },
 ]
+// n: 화살 숫자, info: 어피치 과녁 상태
+const explorer = (n, info, depth, archer) => {
+  // console.log(`n: ${n}, info: ${info}, depth: ${depth}, archer: ${archer}`)
+  console.log('depth', depth)
+  if(n === 0 || depth === 10) {
+    archer[depth] = n;
+    n = 0;
+    return;
+  }
+
+  if(n > info[depth]) {
+    archer[depth] = info[depth]+1;
+    explorer(n-(info[depth]+1), info, depth+1, archer);
+  }
+
+  archer[depth] = 0;
+  explorer(n, info, depth+1, archer);
+}
 
 const solution = (n, info) => {
-  // let cost = info.map((v, idx) => ({ idx, count: v+1, point: (10-idx)*(v ? 2 : 1)/(v+1) })).sort((a, b) => b.point - a.point)
-  let cost = info.reverse();
-  cost = cost.map((v, idx) => ({ idx, count: v+1, point: idx*(v ? 2 : 1)/(v+1)}));
-  
-  console.log('3. cost', cost)
-  cost = cost.sort((a, b) => b.point - a.point);
-  
-
-  console.log('1. cost', cost)
-
-  cost = cost.reduce((acc, {idx, count}, i) => {
-    console.log(idx, count)
-    if(n >= count) {
-      n = n-count;
-      acc.list[idx] = count;
-      acc.ryan = acc.ryan + idx
-    } else {
-      if(count !== 1) {
-        acc.apeach = acc.apeach + idx
-      }
-    }
-
-    return acc;
-  // }, new Array(11).fill(0));
-  }, { list: new Array(11).fill(0), apeach: 0, ryan: 0 });
-
-  console.log('2. cost', cost);
-
-  if(cost.apeach >= cost.ryan) {
-    return [-1]
-  }
-
-  if(n !== 0) {
-    cost.list[0] = n;
-  }
-
-  return cost.list.reverse();
+  explorer(n, info, 0, new Array(11));
 }
 
 const testResult = testcases.map(({n, info, result}) => ({ solution: solution(n, info), result }));
-testResult.forEach(({solution, result}) => console.log(solution, result))
+// testResult.forEach(({info, solution, result}) => console.log(solution, result))
 
 //* 고려 사항
 //? 1. 화살 갯수 
