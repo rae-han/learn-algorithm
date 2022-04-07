29168b75b5562cd992a2811a9ab90b4326d9d23b
Raehan <raehan900809@gmail.com>
2022-04-07 10:29:53 +0900

    modify: function structure

--- programmers/level2/92342_양궁대회.js
+++ programmers/level2/92342_양궁대회.js
@@ -6,28 +6,37 @@ const testcases = [
   // { n: 10, info: [0,0,0,0,0,0,0,0,3,4,3], result: [1,1,1,1,1,1,1,1,0,0,2] },
   // { n: 8, info: [4,0,3,0,2,0,1,0,0,0,0], result: [0,1,0,1,3,1,2,0,0,0,0] },
 ]
-// n: 화살 숫자, info: 어피치 과녁 상태
-const explorer = (n, info, depth, archer) => {
-  // console.log(`n: ${n}, info: ${info}, depth: ${depth}, archer: ${archer}`)
-  console.log('depth', depth)
-  if(n === 0 || depth === 10) {
-    archer[depth] = n;
-    n = 0;
-    return;
-  }
 
-  if(n > info[depth]) {
-    archer[depth] = info[depth]+1;
-    explorer(n-(info[depth]+1), info, depth+1, archer);
-  }
-
-  archer[depth] = 0;
-  explorer(n, info, depth+1, archer);
-}
 
 const solution = (n, info) => {
-  explorer(n, info, 0, new Array(11));
-}
+  const result = [[-1], 0];
+
+  // n: 화살 숫자, info: 어피치 과녁 상태
+  const explorer = (n, apeach, depth, ryan) => {
+    // console.log(`n: ${n}, apeach: ${apeach}, depth: ${depth}, ryan: ${ryan}`)
+    // console.log('depth', depth)
+    if(depth === 0) {
+      // console.log(apeach)
+    }
+
+    if(n === 0 || depth === 10) {
+      ryan[depth] = n;
+      console.log(ryan)
+      compareScore
+      return;
+    }
+
+    if(n > apeach[depth]) {
+      ryan[depth] = apeach[depth]+1;
+      explorer(n-(apeach[depth]+1), apeach, depth+1, ryan);
+    }
+
+    ryan[depth] = 0;
+    explorer(n, apeach, depth+1, ryan);
+  }
+
+  explorer(n, info, 0, Array.from({length: 11}, () => 0));
+};
 
 const testResult = testcases.map(({n, info, result}) => ({ solution: solution(n, info), result }));
 // testResult.forEach(({info, solution, result}) => console.log(solution, result))
