a58041122a11c94dde36d41482dc17f870e35a14
raehan <raehan900809@gmail.com>
2022-10-26 19:09:54 +0900

    solved: 부품 찾기 with 계수 정렬

--- this-is-coding-test/7.이진_탐색/2.부품_찾기.py
+++ this-is-coding-test/7.이진_탐색/2.부품_찾기.py
@@ -3,28 +3,42 @@ requests = [5, 7, 9]
 YES, NO = 'yes', 'no'
 
 
-def binary_search(parts, target, start=0, end=None):
+def binary_search_recursion(parts, target, start=0, end=None):
     end = (len(parts) - 1) if end is None else end
-
     if start > end:
         return NO
 
     middle = int((start + end) / 2)
 
     return YES if parts[middle] == target else (
-        binary_search(parts, target, middle+1, end) if target > parts[middle]
-        else binary_search(parts, target, start, middle-1)
+        binary_search_recursion(parts, target, middle+1, end) if target > parts[middle]
+        else binary_search_recursion(parts, target, start, middle-1)
     )
+
+
+def object_search(parts, requests):
+    dic = dict()
+    result = []
+
+    for part in parts:
+        dic[part] = (dic[part] + 1) if part in dic else 1
+
+    for req in requests:
+        result.append(YES if req in dic else NO)
+
+    return result
 
 
 def search_parts(parts, requests):
     sorted_parts = sorted(parts)
     result = []
 
-    for index in range(len(requests)):
-        result.append(binary_search(sorted_parts, requests[index]))
+    for request in requests:
+        result.append(binary_search_recursion(sorted_parts, request))
 
     print(result)
+
+    print(object_search(parts, requests))
 
 
 search_parts(parts, requests)
