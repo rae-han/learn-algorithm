d320375bba85942dfbecf91fe37c6c1b33bce41b
raehan <raehan900809@gmail.com>
2022-12-07 22:19:13 +0900

    solved: 8_2 dynamic programming

--- this-is-coding-test/8.다이나믹_프로그래밍/2.1로_만들기.py
+++ this-is-coding-test/8.다이나믹_프로그래밍/2.1로_만들기.py
@@ -2,11 +2,21 @@ number = 26
 
 memory = [0] * 3001
 
-for i in range(2, number):
+for i in range(2, number+1):
     memory[i] = memory[i-1] + 1
 
     if i % 2 == 0:
-        memory[i] = min(memory[i])
+        memory[i] = min(memory[i], memory[i//2]+1)
+
+    if i % 3 == 0:
+        memory[i] = min(memory[i], memory[i//3]+1)
+
+    if i % 5 == 0:
+        memory[i] = min(memory[i], memory[i//5]+1)
+
+
+for i in range(2, number+1):
+    print(i, memory[i])
 
 
 
