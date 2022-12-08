7711e960396dfe4c957c1f48878a1e3822c4dfc7
raehan <raehan900809@gmail.com>
2022-12-08 20:44:07 +0900

    solve: efficient money composition other

--- this-is-coding-test/8.다이나믹_프로그래밍/5.효율적인_화폐구성.py
+++ this-is-coding-test/8.다이나믹_프로그래밍/5.효율적인_화폐구성.py
@@ -7,9 +7,12 @@ d = [target] * (target+1)
 d[0] = 0
 
 for m in money:
-    for i in range(0, len(d)+1):
-        if i+m <= target:
-            d[i+m] = min(d[i+m], d[i]+1)
+    for i in range(m, len(d)):
+        print(i)
+        # if i+m <= target:
+        #     d[i+m] = min(d[i+m], d[i]+1)
+        d[i] = min(d[i-m]+1, d[i])
+
 
 print(d[target])
 
