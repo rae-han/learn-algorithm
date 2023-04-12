335e1b4e46615724da1e5126319f5bc4cad3d54b
raehan <raehan900809@gmail.com>
2023-04-12 11:15:20 +0900

    fix: stack func

--- backjoon/10828.스택.js
+++ backjoon/10828.스택.js
@@ -1,61 +1,104 @@
-function Node(data, size, next) {
+const fs = require('fs');
+// env
+const readFileSyncAddress = './dev/stdin';
+// const readFileSyncAddress = "./10828.txt";
+const [n, ...arr] = fs.readFileSync(readFileSyncAddress).toString().trim().split("\n");
+
+function Node(data, next) {
   this.data = data;
-  this.size = size;
   this.next = next;
 }
 
 function Stack() {
   this.peak = null;
+  this.length = 0;
 
   this.push = function (data) {
-    const size = this.size();
-    this.peak = new Node(data, size + 1, this.peak);
+    this.peak = new Node(data, this.peak);
+    this.length += 1;
   };
   this.pop = function () {
-    if (!this.size()) {
+    if (this.length === 0) {
       return -1;
     }
 
-    const data = this.peak.data;
-    this.peak = this.peak.next;
-    return data;
+    const popItem = this.peak;
+    this.peak = popItem.next;
+    this.length -= 1;
+    return popItem.data;
   };
   this.top = function () {
+    if (this.length === 0) {
+      return -1;
+    }
     return this.peak.data;
   };
   this.size = function () {
-    return this.peak ? this.peak.size : 0;
+    return this.length
   };
   this.empty = function () {
-    return this.size() ? 0 : 1;
+    return this.length ? 0 : 1;
   };
 }
 
-function solution(input) {
-  const iter = input[0];
-  const answer = [];
-
-  const stack = new Stack();
-
-  for (let i = 1; i <= iter; i++) {
-    const [cmd, num] = input[i].trim().split(" ");
-    num ? stack[cmd](num) : answer.push(stack[cmd]());
+let answer = [];
+let stack = new Stack();
+const command = arr.map(v=>v.split(' '));
+command.forEach(v=>{
+  switch(v[0]){
+    case 'push':
+      stack.push(v[1])
+      break;
+    case 'pop':
+      answer.push(stack.pop());
+      break;
+    case 'size':
+      answer.push(stack.size())
+      break;
+    case 'empty':
+      answer.push(stack.empty())
+      break;
+    case 'top':
+      answer.push(stack.top())
+      break;
   }
+})
 
-  console.log(answer.join("\n"));
-}
+console.log(answer.join('\n'))
 
-// solution();
+// function solution(iter) {
+//   console.log(iter)
+//   const answer = [];
+//
+//   const stack = new Stack();
+//
+//   const result = iter
+//     .map((item) => item.trim().split(' '))
+//     // .filter(([cmd, num]) => num ? (stack[cmd](num), false) : stack[cmd]())
+//     .filter(([cmd, num]) => {
+//       return num ? false : 1
+//     })
+//   console.log(123, result)
+//
+//   // for (let i = 0; i < iter.length; i++) {
+//   //   const [cmd, num] = iter[i].trim().split(" ");
+//   //   num ? stack[cmd](num) : answer.push(stack[cmd]());
+//   // }
+//
+//   console.log(answer.join("\n"));
+// }
 
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-let input = [];
-rl.on("line", function (line) {
-  input.push(line);
-}).on("close", function () {
-  solution(input);
-  process.exit();
-});
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// let input = [];
+// rl.on("line", function (line) {
+//   if (line !== '') {
+//     input.push(line);
+//   }
+// }).on("close", function () {
+//   solution(input);
+//   process.exit();
+// });
